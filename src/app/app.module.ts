827c4576beb53dc3d22ef82384e9e10338df7463
Teerapong Singthong <st.teerapong@gmail.com>
2017-01-11 10:50:31 +0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './components/app.component';
 import { DisplayItem } from './components/app.displayItem';
 import { DisplayArray } from './components/app.displayArray';
---
+++ src/app/components/app.form.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: '',
+    template: `
+        <div style="border: 1px solid black; border-radius: 5px;>
+
+        </div>
+    `
+})
+
+export class FormApp {
+    constructor() {
+
+    }
+}
